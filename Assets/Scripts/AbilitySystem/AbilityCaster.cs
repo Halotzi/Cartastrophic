a5ff98a1b7a12fe138da346536ae402d1351ea19
Jonnix12 <jonharel48@gmail.com>
2023-05-14 14:13:10 +0300

    Add ability system

---
+++ Assets/Scripts/AbilitySystem/AbilityCaster.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using AbilitySystem;
+using UnityEngine;
+
+public class AbilityCaster : MonoBehaviour
+{
+    private List<BaseAbility> _abilities;
+
+    private void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/AbilitySystem/BaseAbility.cs
@@ -0,0 +1,15 @@
+﻿namespace AbilitySystem
+{
+    public abstract class BaseAbility
+    {
+        public string AbilityName { get; protected set; }
+        public AbilityCaster Caster { get; set; }
+
+        protected BaseAbility(AbilityCaster caster)
+        {
+            Caster = caster;
+        }
+        
+        public abstract void Cast();
+    }
+}
