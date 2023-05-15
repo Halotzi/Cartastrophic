d58d141a55f14049dc8b76188e3491b4cc1feed4
Liad Halotzi <liadhalotzi@gmail.com>
2023-05-15 19:00:53 +0300

    Almost finished controller

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CarController : MonoBehaviour
 {
@@ -9,47 +10,158 @@ public class CarController : MonoBehaviour
     private float currentSteerAngle, currentbreakForce;
     private bool isBreaking;
 
-    // Settings
-    [SerializeField] private float motorForce, breakForce, maxSteerAngle;
+    [Header("Input Action Assets")]
+    [SerializeField] InputActionMap _playerInputActionMap;
+    private InputActionAsset _inputAsset;
+    private PlayerController _playerController;
 
-    // Wheel Colliders
-    [SerializeField] private WheelCollider frontLeftWheelCollider, frontRightWheelCollider;
-    [SerializeField] private WheelCollider rearLeftWheelCollider, rearRightWheelCollider;
+    public event Action<PlayerInput> onPlayerJoined;
+    public event Action<PlayerInput> onPlayerLeft;
 
-    // Wheels
-    [SerializeField] private Transform frontLeftWheelTransform, frontRightWheelTransform;
-    [SerializeField] private Transform rearLeftWheelTransform, rearRightWheelTransform;
+    [Header("Settings")]
+    [SerializeField] private float motorForce;
+    [SerializeField] private float breakForce;
+    [SerializeField] private float maxSteerAngle;
+
+    [Header("Wheel Colliders")]
+    [SerializeField] private WheelCollider frontLeftWheelCollider;
+    [SerializeField] private WheelCollider frontRightWheelCollider;
+    [SerializeField] private WheelCollider rearLeftWheelCollider;
+    [SerializeField] private WheelCollider rearRightWheelCollider;
+
+    [Header("Wheels")]
+    [SerializeField] private Transform frontLeftWheelTransform;
+    [SerializeField] private Transform frontRightWheelTransform;
+    [SerializeField] private Transform rearLeftWheelTransform;
+    [SerializeField] private Transform rearRightWheelTransform;
+
+    [Header("Movement fields")]
+    [SerializeField] Rigidbody _rigidbody;
+    [SerializeField] float _movmentForce=5f;
+    [SerializeField] float _maxSpeed=5f;
+    private Vector3 _forceDirection = Vector3.zero;
+
+    [Header("Camera")]
+    [SerializeField] Camera _camera;
+
+    #region InputActions
+    private InputAction _rotateInputAction;
+    private InputAction _breakInputAction;
+    #endregion
+
+    #region InputAction bools
+    private bool _isRotateEnabled = true;
+    private bool _isGassPressed = false;
+    #endregion
+
+    private void Awake()
+    {
+        _inputAsset = this.GetComponent<PlayerInput>().actions;
+        _playerInputActionMap = _inputAsset.FindActionMap("Player");
+        _playerController = new PlayerController();
+        EnableInputAction();
+    }
+
+    private void OnEnable()
+    {
+        //_playerInputActionMap.FindAction("Rotate").started += RotateCar;
+        _playerInputActionMap.FindAction("Gass").performed += PressGass;
+        _playerInputActionMap.FindAction("Gass").canceled += CancleGass;
+        _playerInputActionMap.FindAction("Break").performed += StartBreak;
+        //_playerInputActionMap.FindAction("Gass").canceled += StartBreak;
+    }
+
+   
+
+    private void OnDisable()
+    {
+        //_playerInputActionMap.FindAction("Rotate").started -= RotateCar;
+        _playerInputActionMap.FindAction("Gass").performed -= PressGass;
+        _playerInputActionMap.FindAction("Gass").canceled -= CancleGass;
+        _playerInputActionMap.FindAction("Break").performed -= StartBreak;
+        //_playerInputActionMap.FindAction("Gass").canceled -= StartBreak;
+
+    }
 
     private void FixedUpdate()
     {
         GetInput();
+        if(_isGassPressed)
         HandleMotor();
         HandleSteering();
         UpdateWheels();
+
+        //if (_isRotateEnabled)
+        //{
+        //    _forceDirection += _rotateInputAction.ReadValue<Vector2>().x * GetCameraForward(_camera) * _movmentForce;
+        //    _forceDirection += _rotateInputAction.ReadValue<Vector2>().y * GetCameraRight(_camera) * _movmentForce;
+
+        //    _rigidbody.AddForce(_forceDirection,ForceMode.Impulse);
+        //    _forceDirection = Vector3.zero;
+
+        //    if(_isGassPressed)
+        //    {
+        //        if (_rigidbody.velocity.y < 0f)
+        //            _rigidbody.velocity += Vector3.down * Physics.gravity.y * Time.fixedDeltaTime;
+
+        //        Vector3 horizontalVelocity = _rigidbody.velocity;
+        //        horizontalVelocity.y = 0f;
+
+        //        if (horizontalVelocity.sqrMagnitude > _maxSpeed * _maxSpeed)
+        //            _rigidbody.velocity = horizontalVelocity.normalized * _maxSpeed + Vector3.up * _rigidbody.velocity.y;
+        //    }
+        //}
+    }
+
+    private Vector3 GetCameraForward(Camera playerCamera)
+    {
+        Vector3 forward = playerCamera.transform.forward;
+        forward.y = 0f;
+        return forward.normalized;
+    }
+
+    private Vector3 GetCameraRight(Camera playerCamera)
+    {
+        Vector3 right = playerCamera.transform.right;
+        right.y = 0f;
+        return right.normalized;
+    }
+
+    private void EnableInputAction()
+    {
+        _rotateInputAction = _playerController.Player.Rotate;
+        _breakInputAction = _playerController.Player.Gass;
+        _breakInputAction = _playerController.Player.Break;
+
+        _rotateInputAction.Enable();
+        _breakInputAction.Enable();
     }
 
     private void GetInput()
     {
         // Steering Input
-        horizontalInput = Input.GetAxis("Horizontal");
+        //horizontalInput = Input.GetAxis("Horizontal");
+        horizontalInput = _rotateInputAction.ReadValue<Vector2>().x;
 
-        // Acceleration Input
-        verticalInput = Input.GetAxis("Vertical");
+        //// Acceleration Input
+        //verticalInput = Input.GetAxis("Vertical");
+        verticalInput = 0;
 
         // Breaking Input
-        isBreaking = Input.GetKey(KeyCode.Space);
+        //isBreaking = Input.GetKey(KeyCode.Space);
     }
 
     private void HandleMotor()
     {
-        frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
-        frontRightWheelCollider.motorTorque = verticalInput * motorForce;
+        frontLeftWheelCollider.motorTorque = motorForce;
+        frontRightWheelCollider.motorTorque = motorForce;
         currentbreakForce = isBreaking ? breakForce : 0f;
         ApplyBreaking();
     }
 
     private void ApplyBreaking()
     {
+        Debug.Log("StartBreak");
         frontRightWheelCollider.brakeTorque = currentbreakForce;
         frontLeftWheelCollider.brakeTorque = currentbreakForce;
         rearLeftWheelCollider.brakeTorque = currentbreakForce;
@@ -78,5 +190,24 @@ public class CarController : MonoBehaviour
         wheelCollider.GetWorldPose(out pos, out rot);
         wheelTransform.rotation = rot;
         wheelTransform.position = pos;
+    }
+
+    private void RotateCar(InputAction.CallbackContext obj)
+    {
+    }
+
+    private void PressGass(InputAction.CallbackContext obj)
+    {
+        _isGassPressed = true;
+    }
+
+    private void CancleGass(InputAction.CallbackContext obj)
+    {
+        _isGassPressed = false;
+    }
+
+    private void StartBreak(InputAction.CallbackContext obj)
+    {
+        ApplyBreaking();
     }
 }
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerManager : MonoBehaviour
+{
+   private List<PlayerInput> playerInputs = new List<PlayerInput>();
+
+    [SerializeField]
+    private List<Transform> startingPosition;
+
+    [SerializeField]
+    private List<LayerMask> playerLayers;
+
+    private PlayerInputManager playerInputManager;
+
+    private void Awake()
+    {
+        playerInputManager = FindObjectOfType<PlayerInputManager>();
+    }
+
+    private void OnEnable()
+    {
+        playerInputManager.onPlayerJoined += AddPlayer;
+    }
+
+    private void OnDisable()
+    {
+        playerInputManager.onPlayerJoined -= AddPlayer;
+    }
+
+    public void AddPlayer(PlayerInput player)
+    {
+        playerInputs.Add(player);
+        player.transform.position = startingPosition[playerInputs.Count - 1].position;
+    }
+}
